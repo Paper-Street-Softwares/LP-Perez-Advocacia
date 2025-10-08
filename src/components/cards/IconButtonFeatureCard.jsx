996b741ec494d52dae87da1b0ac09da9adb808f5
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-08 14:41:20 -0300

    fix: Ajustes

--- src/components/cards/IconButtonFeatureCard.jsx
+++ src/components/cards/IconButtonFeatureCard.jsx
@@ -32,7 +32,7 @@ export default function IconButtonFeatureCard(props) {
       </h1>
 
       <p
-        className={`text-center opacity-70 font-mainFont w-[90%] pb-4 ${textClass}`}
+        className={`text-center opacity-70 font-secondFont w-[90%] pb-4 ${textClass}`}
       >
         {paragraph}
       </p>
--- src/components/sections/FeaturesWithIcons.jsx
+++ src/components/sections/FeaturesWithIcons.jsx
@@ -36,7 +36,7 @@ export default function FeaturesWithIcons({ colorMode }) {
         colorMode="dark"
       />
       <SectionWrapper>
-        <div className="flex desktop1:mt-[40px] flex-col items-center w-full tablet1:flex-row tablet1:justify-between desktop1:gap-x-0 desktop1:px-0">
+        <div className="flex flex-col items-center w-full tablet1:flex-row tablet1:justify-between desktop1:gap-x-0 desktop1:px-0">
           <div className="col1 tablet1:w-[50%] desktop1:w-[28%] flex flex-col justify-center items-center">
             <MotionDivDownToUp>
               <IconFeatureCard
@@ -62,7 +62,7 @@ export default function FeaturesWithIcons({ colorMode }) {
             <img
               src={content.texts.features.imgFeatures}
               alt={content.texts.features.alt}
-              className="hidden h-[640px] desktop1:h-[900px] object-cover w-full desktop1:flex col2 rounded-2xl bg-top bg-cover shadow-custom-opacity shadow-shadowFeatures/10"
+              className="hidden h-[640px] desktop1:h-[640px] object-cover w-full desktop1:flex col2 rounded-2xl bg-top bg-cover shadow-custom-opacity shadow-shadowFeatures/10"
               loading="lazy"
             />
           </MotionDivDownToUp>
