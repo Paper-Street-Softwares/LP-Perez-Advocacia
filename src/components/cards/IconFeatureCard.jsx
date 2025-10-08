b8dd44a73417ecc232ee6d25137ae13549e18916
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-08 18:19:51 -0300

    fix: Features

--- src/components/cards/IconFeatureCard.jsx
+++ src/components/cards/IconFeatureCard.jsx
@@ -19,7 +19,7 @@ export default function IconFeatureCard(props) {
 
   return (
     <div
-      className={`w-full tablet1:min-h-[200px] p-0 tablet1:h-[250px] tablet1:mt-0 desktop1:w-[230px] desktop1:h-[300px] flex flex-col items-center desktop1:hover:scale-110 transition desktop1:p-0 ${className}`}
+      className={`w-full tablet1:min-h-[200px] p-0 tablet1:h-[260px] tablet1:mt-0 desktop1:w-[230px] desktop1:h-[300px] flex flex-col items-center desktop1:hover:scale-110 transition desktop1:p-0 ${className}`}
     >
       <div
         className={`h-[64px] w-[64px] mb-[24px] rounded-md flex justify-center items-center text-labelButtons ${bgClass}`}
