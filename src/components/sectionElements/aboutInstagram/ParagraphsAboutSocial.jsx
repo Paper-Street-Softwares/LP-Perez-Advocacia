c815428e567a249e057823914e424f3adafcd8e2
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-08 14:44:39 -0300

    fix: Ajustes

--- src/components/sectionElements/aboutInstagram/ParagraphsAboutSocial.jsx
+++ src/components/sectionElements/aboutInstagram/ParagraphsAboutSocial.jsx
@@ -2,12 +2,15 @@ import Paragraphs from "../Paragraphs";
 import React, { useState } from "react";
 import content from "../../../content/content";
 import MotionDivDownToUp from "../../animation/MotionDivDownToUp";
+import { useTranslation } from "react-i18next";
 
 function ParagraphsAboutSocial() {
+  const { t } = useTranslation();
+
   return (
     <MotionDivDownToUp>
-      <Paragraphs className="text-colorWhite text-opacity-80 mb-[48px]">
-        {content.texts.about.aboutSocial.paragraph}
+      <Paragraphs className="text-white text-opacity-80 mb-[48px]">
+        {t("about.aboutSocial.subtitle")}
       </Paragraphs>
     </MotionDivDownToUp>
   );
